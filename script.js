e384ef381d333f9332c444c323a244ab64aa4894
George <georgij.brovko@gmail.com>
2024-11-07 01:52:47 +0700

    Добавлены изменения_2

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
+document.getElementById("CalculatePer").onclick = () => {
 let a = parseFloat(prompt('Введите сторону квадрата:'));
         if (!isNaN(a)) {
-            let P = Math.pow(4 * a, 2);
+            let P = Math.pow(4 * a, 2).toFixed(2);
             alert('Квадрат периметра квадрата: ' + P);
         } else {
             alert('Введите корректное число');
         }
+    };
 
+document.getElementById("CalculateMetrs").onclick = () => {
 let L = parseFloat(prompt('Введите расстояние в сантиметрах:'));
-if (Number.isInteger(L) && L >= 0) {
-    let meters = Math.floor(L / 100);
-    alert('Количество полных метров: ' + meters);
-} else {
-    alert('Введите корректное целое число');
-}
-
+        if (Number.isInteger(L) && L >= 0) {
+            let meters = Math.floor(L / 100);
+            alert('Количество полных метров: ' + meters);
+        } else {
+            alert('Введите корректное целое число');
+        }
+    };
   
